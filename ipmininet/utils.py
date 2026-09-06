9a867d911794e6f0a8903b78c67ca1f5baf06b18
Iakov Kirilenko <jake.kirilenko@gmail.com>
2026-09-06 14:28:53 +0300

    style(utils): ruff format find_node

--- ipmininet/utils.py
+++ ipmininet/utils.py
@@ -173,6 +173,7 @@ def find_node(start: Node, node_name: str) -> Intf | None:
 
     if start.name == node_name:
         return start.intf()
+
     def _expand(i):
         return [
             intf
@@ -180,6 +181,7 @@ def find_node(start: Node, node_name: str) -> Intf | None:
             if L3Router.is_l3router_intf(n)
             for intf in realIntfList(n.node)
         ]
+
     for i in walk_unvisited(realIntfList(start), _expand):
         for n in i.broadcast_domain.interfaces:
             if n.node.name == node_name:
